dc9f5399f63aeea4e8a9261faa4de5a28bfa794a
mth3428 <mth3428@gmail.com>
2022-12-06 23:30:36 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        
+  validates :nickname, presence: true
+  validates :introduction, presence: true
+
+  # パスワードは半角英数字混合での入力が必須
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+  with_options presence: true, format: { with: PASSWORD_REGEX }do
+    validates :password
+  end
 end
