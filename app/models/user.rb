b011a61144ac5d36950d98d51e0a8731fe39ae69
mth3428 <mth3428@gmail.com>
2022-12-07 23:39:51 +0900

    アカウントの編集機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   # パスワードは半角英数字混合での入力が必須
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
 
-  with_options presence: true, format: { with: PASSWORD_REGEX }do
+  with_options presence: true, format: { with: PASSWORD_REGEX }, on: :create do
     validates :password
   end
 end
