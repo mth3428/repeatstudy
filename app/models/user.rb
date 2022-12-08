66d1355d3cba4224329c57e1dfe992fddeb58d3f
mth3428 <mth3428@gmail.com>
2022-12-08 22:55:15 +0900

    新規登録とログインの画面遷移の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
         
+  has_many :posts
+
   validates :nickname, presence: true
   validates :introduction, presence: true
 
