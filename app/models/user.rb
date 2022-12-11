e252044bb1f75202ad2083a648de4df6c2c21f1c
mth3428 <mth3428@gmail.com>
2022-12-12 08:37:41 +0900

    画像の表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
         
 
-        class User < ActiveRecord::Base
-          mount_uploader :image, ImageUploader
-        end
+        
   
   has_many :posts
   has_one_attached :image
