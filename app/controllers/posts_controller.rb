55bba0db6e6ff40f302f7ca78fa1d678948493bb
mth3428 <mth3428@gmail.com>
2022-12-12 22:34:52 +0900

    アクションとルーティングを設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,14 @@ class PostsController < ApplicationController
   def index
   end
 
+  def new
+    
+  end
+
+  def create
+    
+  end
+
   private
 
   def message_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
+
+  resources :posts, only: [:new, :create]
   resources :users, only: [:new, :edit, :update]
 end
