714165b596c9b9f6fd6ebb935be9d1a698a59b3b
mth3428 <mth3428@gmail.com>
2022-12-17 12:33:20 +0900

    ツイート詳細、編集、削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
-  
+  before_action :move_to_index, except: [:index, :show]
   def index
     @posts = Post.all.order(created_at: "DESC")
     
@@ -20,8 +20,31 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
 
   end
+
+  def edit
+    @post = Post.find(params[:id])
+    redirect_to root_path unless current_user.id == @post.user_id
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      redirect_to root_path
+    end
+  end
+
 
   private
 
@@ -29,4 +52,10 @@ class PostsController < ApplicationController
     params.require(:post).permit(:tweet, :image).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+  
 end
