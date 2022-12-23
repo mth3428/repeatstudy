78ac101969a7d8ae038ea388c01894047703c3b1
mth3428 <mth3428@gmail.com>
2022-12-23 15:20:45 +0900

    アソシエーーションの記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :comments, dependent: :destroy
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
 end
