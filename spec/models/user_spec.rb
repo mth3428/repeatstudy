89ff38889365c26014b2ae4e8ff4b06567019e0a
mth3428 <mth3428@gmail.com>
2023-01-08 12:23:42 +0900

    deviseの日本語化

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe User, type: :model do
   describe "ユーザー新規登録" do
     context "登録できないとき" do
       it "nicknameが空だと登録できない" do
+        user = FactoryBot.build(:user)
+        user.nickname = ""
+        user.valid?
+        expect(user.errors.full_messages).to include "Nickname translation missing: ja.activerecord.errors.models.user.attributes.nickname.blank"
       end
       it "emailが空では登録できない" do
       end
