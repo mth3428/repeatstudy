b03b8953a1ea7aa85c48449ddeb6ada1de43b45a
mth3428 <mth3428@gmail.com>
2023-02-07 22:05:55 +0900

    プレビュー機能の実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -5,5 +5,23 @@ document.addEventListener('DOMContentLoaded', function(){
   const previewList = document.getElementById('previews');
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
   if (!postForm) return null;
-  console.log("preview.jsが読み込まれました");
+
+  // input要素を取得
+  const fileField = document.querySelector('input[type="file"][name="post[images][]"]');
+  // input要素で値の変化が起きた際に呼び出される関数
+  fileField.addEventListener('change', function(e){
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+    // 画像を表示するためのdiv要素を生成
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+    // 表示する画像を生成
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+
+    // 生成したHTMLの要素をブラウザに表示させる
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
+  });
 });
